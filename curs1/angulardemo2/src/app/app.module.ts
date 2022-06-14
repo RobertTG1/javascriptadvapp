216b8bc983554a036ec4ad7f8dc621494e0a1561
RobertTG1 <robert.moraru.luca@gmail.com>
2022-06-14 20:58:31 +0300

    materiale

--- curs1/angulardemo2/src/app/app.module.ts
+++ curs1/angulardemo2/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/containers/home/home.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { AdminComponent } from './admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
