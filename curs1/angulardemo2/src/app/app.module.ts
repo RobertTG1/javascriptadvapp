a612aa4933136a9f18df2ee0632fe6584c7a2e5d
RobertTG1 <robert.moraru.luca@gmail.com>
2022-06-14 20:38:14 +0300

    jsdevapp

---
+++ curs1/angulardemo2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/containers/home/home.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    XyzComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
